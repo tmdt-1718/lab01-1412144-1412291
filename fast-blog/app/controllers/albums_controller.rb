3774d61f24cb31e16e2e3e46512d064b09b94f2e
1412144 <truonghai.itus@gmail.com>
2017-09-30 21:01:44 +0700

    generate view

--- fast-blog/app/controllers/albums_controller.rb
+++ fast-blog/app/controllers/albums_controller.rb
@@ -1,2 +1,45 @@
 class AlbumsController < ApplicationController
+
+  def new
+    @album = Album.new
+  end
+  def show
+    @album = Album.find(params[:id])
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+  end
+  def index
+    @albums = Album.where('status = 0')
+  end
+  def create
+    @album = current_user.albums.new(album_params)
+    if @album.save
+      redirect_to @album
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @album = Album.find(params[:id])
+    if @album.update(album_params)
+      redirect_to @album
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @album = Album.find(params[:id])
+    @album.destroy
+
+    redirect_to albums_path
+  end
+
+  private
+    def album_params
+      params.require(:album).permit(:name)
+    end
 end
